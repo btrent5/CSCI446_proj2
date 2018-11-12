f922d73288c05aee8272e1e0552d66deeb83ac9f
trent <b.trent5@gmail.com>
2018-11-12 12:59:21 -0700

    refactored into interface and scaled outputs

--- src/BasicBacktrack.kt
+++ src/BasicBacktrack.kt
@@ -1,69 +1,15 @@
 class BasicBacktrack : BacktrackAlg {
 
-    override fun search(maze: MutableList<MutableList<Node>>) : Boolean
-    {
-        // if assignment is complete then return
-        if(isComplete(maze)){
-            print("Assignment complete")
-            return true
-        }
-        var node = SelectUnassignedVariable(maze)
-        // for each value in OrderDomainValue(var, assignment, csp)
-        for (value in OrderDomainValue(node)) {
-            node.type = value
-
-            //renderMaze(maze, "res/output/temp.png")
-            // if value is consistent with assignment then
-            val consistent =  node.neighbors.filter { neighbor -> neighbor.type != '?' }.all { neighbor -> neighbor.validateConstraints() }
-            //val consistent =  maze.all { row -> row.all { node -> node.validateConstraints() } }
-
-            if(consistent) {
-                // add {var = value} to assignment
-                // inferences = Inference(csp, var, value)
-                // if inferences != failure then
-                // add inferences to assignment
-                var result = search(maze)
-                // if result != failure then
-                if(result) {
-                    return true
-                }
-                // remove { var = value} and inferences from assignment
-            }
-            node.type = '_'
-        }
-        // return failure
-        return false
-    }
-
-    // SelectUnassignedVariable will be first variable found unassigned
-    // Need function to check for assignment being complete
-    // OrderDomainValue can just return an unordered list of domain values
-
     /*
-        Returns true if the assignment is complete, false otherwise.
+    Selects based on location in the puzzle
+    most northwesterly unassigned node will be selected first
      */
-    fun isComplete(maze : MutableList<MutableList<Node>>) : Boolean
-    {
-        return maze.all { row -> row.all { node -> node.type != '_' } }
-    }
-
-    /*
-        Selects an unassigned node from the maze
-     */
-    fun SelectUnassignedVariable(maze : MutableList<MutableList<Node>>) : Node
-    {
+    override fun selectUnassignedVariable(maze: MutableList<MutableList<Node>>): Node {
         var temp = mutableListOf<MutableList<Node>>()
         temp.addAll(maze)
-        temp = temp.map { row -> row.filter { node -> node.type == '_' }.toMutableList() }.filter { row -> !row.isEmpty() }.toMutableList()
+        temp = temp.map { row -> row.filter { node -> node.type == '_' }.toMutableList() }
+                .filter { row -> !row.isEmpty() }.toMutableList()
         return temp[0][0]
-    }
-
-    /*
-        Returns domain of node
-     */
-    fun OrderDomainValue(node : Node) : MutableList<Char>
-    {
-        return node.domain
     }
 }
 
--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,26 @@
 import java.io.File
 
 //PUZZLE_PATH can be either a file or a directory
-const val PUZZLE_PATH = "res/puzzles/12x12maze.txt"
+const val PUZZLE_PATH = "res/puzzles/"
 const val OUTPUT_PATH = "res/output/"
 
 fun main(args: Array<String>) {
     val startTime = System.currentTimeMillis()
-    val file = File(PUZZLE_PATH)
-    val maze = Maze(file.path)
-    renderMaze(maze.nodes,
-            "$OUTPUT_PATH${file.name.removeSuffix("maze.txt")}_input.png")
-    val backtrack : BacktrackAlg = BasicBacktrack()
-    backtrack.search(maze.nodes)
-    renderMaze(maze.nodes,
+    File(PUZZLE_PATH).walk().forEach { file ->
+//        only try to import text files
+        if (file.path.endsWith(".txt")) {
+            var currentMaze = Maze(file.path).nodes
+//            render input state
+            renderMaze(currentMaze,
+                    "$OUTPUT_PATH${file.name.removeSuffix("maze.txt")}_input.png")
+//            find solution
+            BasicBacktrack().search(currentMaze)
+//            render solution
+            renderMaze(currentMaze,
                     "$OUTPUT_PATH${file.name.removeSuffix("maze.txt")}_output.png")
-//    run on each file in PUZZLE_PATH
-//    File(PUZZLE_PATH).walk().forEach { file ->
-//        //        only try to import text files
-//        if (file.path.endsWith(".txt")) {
-//            renderMaze(Maze(file.path).nodes,
-//                    "$OUTPUT_PATH${file.name.removeSuffix("maze.txt")}_input.png")
-//        }
-//        print("Looping")
-//    }
-    println("I ran")
-    println("render finished: ${System.currentTimeMillis() - startTime}ms\n")
+        }
+    }
+    println("Solved $PUZZLE_PATH in ${System.currentTimeMillis() - startTime}ms\n")
 }
 
 
