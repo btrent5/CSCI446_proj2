b62c1ec0acff872800cf76a3dfb00bfaf241b86c
Penchant <dylan.m.wickham@gmail.com>
2018-11-15 22:08:12 -0700

    Fixed assignments being cumulative. Added commandline configuration option

--- src/Main.kt
+++ src/Main.kt
@@ -2,16 +2,28 @@ import java.io.File
 import kotlin.system.measureTimeMillis
 
 //PUZZLE_PATH can be either a file or a directory
-const val PUZZLE_PATH = "res/puzzles/"
-const val OUTPUT_PATH = "res/output/"
+var PUZZLE_PATH = "res/puzzles"
+var OUTPUT_PATH = "res/output"
 
 fun main(args: Array<String>) {
+    var backtrack : BacktrackAlg = SmartBacktrack()
+    if (args.count() > 0){
+        PUZZLE_PATH = args[0]
+    }
+    if(args.count() > 1){
+        OUTPUT_PATH = args[1]
+    }
+    if (args.count() > 2){
+        if (args[2].toLowerCase().equals("true")){
+            backtrack = BasicBacktrack()
+        }
+    }
 
-    var backtrack = BasicBacktrack()
     val startTime = System.currentTimeMillis()
     File(PUZZLE_PATH).walk().forEach { file ->
         //        only try to import text files
         if (file.path.endsWith(".txt")) {
+            backtrack.assignments = 0
             var currentMaze = Maze(file.path).nodes
 //            render input state
             renderMaze(currentMaze,
